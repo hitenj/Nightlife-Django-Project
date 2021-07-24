2965c35fd2a8568eb1e351a39245dfd17a3ef129
Hiten Jain <hgeetaj@gmail.com>
2021-07-25 01:27:06 +0530

    update

--- nightlife/settings.py
+++ nightlife/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #manually added
 STATICFILES_DIRS = [
