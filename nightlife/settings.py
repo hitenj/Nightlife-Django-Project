8e44b861a52b99f64e990749f5a59df0b0ead580
Hiten Jain <hgeetaj@gmail.com>
2021-08-05 00:01:19 +0530

    Update settings.py

--- nightlife/settings.py
+++ nightlife/settings.py
@@ -105,8 +105,7 @@ WSGI_APPLICATION = 'nightlife.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'dc847lsc068j86',
